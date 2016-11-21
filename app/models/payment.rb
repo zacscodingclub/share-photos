009fe23be7f337bc650b7a3b596a169928035219
zacscodingclub <zbaston@gmail.com>
2016-11-20 19:24:14 -0500

    add payment forms

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,11 +2,11 @@ class Payment < ApplicationRecord
   attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
   belongs_to :user
 
-  def self.month
+  def self.months
     Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1] }
   end
 
-  def self.year
+  def self.years
     (Date.today.year..(Date.today.year + 10)).to_a
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one :payment
-  accept_nested_attributes_for :payment
+  accepts_nested_attributes_for :payment
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
