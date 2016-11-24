2bc4bfb46741a7ed0a846c473055c4c1691b1b86
zacscodingclub <zbaston@gmail.com>
2016-11-24 11:27:41 -0500

    basic image upload completed, still need to work on this feature

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,47 @@
+class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    @image.user = current_user
+    binding.pry
+    if @image.save
+      redirect_to @image, notice: "Image was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:name, :picture, :user_id)
+    end
+
+    def set_image
+      @image ||= Image.find(params[:id])
+    end
+end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ApplicationRecord
   belongs_to :user
+  mount_uploader :picture, PictureUploader
+  validates_presence_of :name, :picture
 end
