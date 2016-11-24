7ff7854b202d45d2e563f92db8faf043442c1822
zacscodingclub <zbaston@gmail.com>
2016-11-24 12:31:09 -0500

    fix syntax in carrier_wave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credientials = {
                  :provider => 'AWS',
-        :aws_access_key_id => ENV['AWS_SHARE_PHOTOS_ID']
+        :aws_access_key_id => ENV['AWS_SHARE_PHOTOS_ID'],
     :aws_secret_access_key => ENV['AWS_SHARE_PHOTOS_SECRET']
     }
     config.fog_directory = ENV['S3_BUCKET']
