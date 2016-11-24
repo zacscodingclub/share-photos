a725d5e770fa813418f4c872cd6b7d05c0f72568
zacscodingclub <zbaston@gmail.com>
2016-11-24 12:01:40 -0500

    enforce size validations on image upload.  update image#index style

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,12 @@ class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
   validates_presence_of :name, :picture
+  validate :picture_size
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
